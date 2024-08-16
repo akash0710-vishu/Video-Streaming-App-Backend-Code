7ae13a07b8ddefbb17a601a0239765dd87ede5ca
akash0710-vishu <av4456ts@gmail.com>
2024-08-16 14:38:59 +0530

    VSABC

---
+++ src/main/java/com/stream/app/entities/Course.java
@@ -0,0 +1,18 @@
+package com.stream.app.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "yt_courses")
+public class Course {
+
+    @Id
+    private String id;
+
+    private String title;
+
+}
---
+++ src/main/java/com/stream/app/repositories/VideoRepositories.java
@@ -0,0 +1,15 @@
+package com.stream.app.repositories;
+
+import com.stream.app.entities.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface VideoRepositories extends JpaRepository<Video,String> {
+
+    Optional<Video> findByTitle(String title);
+
+}
